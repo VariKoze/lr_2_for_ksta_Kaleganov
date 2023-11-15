3b87fbb213df292c866ec901d803eff7bdacff51
kaleganov andrey <andreykaleganov50@gmail.com>
2023-11-15 21:26:32 +0300

    test something

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
 using namespace std;
 
-
-int main() {
-    int N;
-    cout << "Введите количество итераций N: ";
-    cin >> N;
+int fibbonachi(int N)
+{
+    int ticker = 0;
     int a = 0, b = 1, nextTerm;
     cout << "Ряд чисел Фибоначчи до " << N << " итераций: ";
     for (int i = 1; i <= N; ++i) {
-        if (i%3==0)
+        if (ticker==3)
         {
             cout << a << " ";
+            ticker = 0;
         }
         nextTerm = a + b;
         a = b;
         b = nextTerm;
+        ticker++;
     }
     return 0;
+}
+int main() {
+    int iteration = 0;
+    cout<<"Введите количество итераций N:";
+    cin>>iteration;
+    fibbonachi(iteration);
 }
